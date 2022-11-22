ca930fd302003cb95c8ed0688067ff9e39fe01d8
배지 <ttsbu@naver.com>
2022-11-22 22:48:58 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import UseEffect from './components/UseEffect';
 import CleanUp from './components/CleanUp';
 import Todo from './components/Todo';
+import CoinTracker from './components/CoinTracker';
 
 function App() {
   return (
     <>
       {/* <UseEffect />; */}
       {/* <CleanUp /> */}
-      <Todo />
+      {/* <Todo /> */}
+      <CoinTracker />
     </>
   );
 }
